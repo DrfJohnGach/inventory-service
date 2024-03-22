a10d5f70ff5791814c6481929a07849898a32aa0
ash-ina <johngachungagitau@gmail.com>
2024-03-22 13:21:15 +0300

    Inventory service implementation

---
+++ src/main/java/com/drfjohngach/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,21 @@
+package com.drfjohngach.inventoryservice.controller;
+
+import com.drfjohngach.inventoryservice.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping("/{skuCode}")
+    @ResponseStatus(HttpStatus.OK)
+    public Boolean isInStock(@PathVariable("SkuCode") String skuCode){
+        return inventoryService.isInStock(skuCode);
+    }
+}
