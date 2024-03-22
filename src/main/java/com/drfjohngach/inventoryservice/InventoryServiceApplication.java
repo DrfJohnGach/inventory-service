191b8262c4660ee1380a22aeadef2a5076a2ce1d
drfjohngach <johngachungagitau@gmail.com>
2024-03-22 13:51:05 +0300

    Adding data to the inventory database using @Bean

--- src/main/java/com/drfjohngach/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/com/drfjohngach/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package com.drfjohngach.inventoryservice;
 
+import com.drfjohngach.inventoryservice.model.Inventory;
+import com.drfjohngach.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,4 +14,24 @@ public class InventoryServiceApplication {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner(InventoryRepository inventoryRepository){
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("Iphone_13");
+			inventory.setQuantity(100);
+
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("HP_notebook_G6");
+			inventory1.setQuantity(10);
+
+			Inventory inventory2 = new Inventory();
+			inventory2.setSkuCode("Iphone_13_red");
+			inventory2.setQuantity(0);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory1);
+			inventoryRepository.save(inventory2);
+		};
+	}
 }
--- src/main/java/com/drfjohngach/inventoryservice/controller/InventoryController.java
+++ src/main/java/com/drfjohngach/inventoryservice/controller/InventoryController.java
@@ -3,7 +3,6 @@ package com.drfjohngach.inventoryservice.controller;
 import com.drfjohngach.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
